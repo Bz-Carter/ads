98a6aab79299e8f316f945ffd018ad5116d13472
Bz-Carter <lebz2016@gmail.com>
2021-10-24 22:49:19 +0000

    single component created .

--- src/app/pages/event/event.module.ts
+++ src/app/pages/event/event.module.ts
@@ -1,24 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LightgalleryModule } from 'lightgallery/angular/11';
 
 import { EventRoutingModule } from './event-routing.module';
 import { EventComponent } from './event.component';
 import { CalendrierComponent } from './calendrier/calendrier.component';
 import { FilterPipe } from './pipes/filter.pipe';
+import { SingleComponent } from './single/single.component';
 
 
 @NgModule({
   declarations: [
     EventComponent,
     CalendrierComponent,
-    FilterPipe
+    FilterPipe,
+    SingleComponent,
   ],
   imports: [
     CommonModule,
     EventRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    LightgalleryModule
   ]
 })
 export class EventModule { }
