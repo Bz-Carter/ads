7c321aa9307001c4cab37b1022e8050c9dca392d
Bz-Carter <lebz2016@gmail.com>
2021-10-24 18:21:13 +0000

    Events Filters created .

--- src/app/pages/event/event.component.ts
+++ src/app/pages/event/event.component.ts
@@ -8,24 +8,25 @@ import { MediaService } from 'src/app/services/media.service';
 @Component({
   selector: 'app-event',
   templateUrl: './event.component.html',
-  styleUrls: ['./event.component.scss']
+  styleUrls: ['./event.component.scss'],
 })
 export class EventComponent implements OnInit {
   categories: Category[] = [];
   public events: Media[] = [];
 
   public searchTerm: string = '';
-  searchKey:string = "";
-  
-  
+  searchKey: string = '';
+
+  public filterCategory: any;
+
   constructor(
     private categoryService: CategoryService,
     private mediaService: MediaService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.refresh();
-    this.mediaService.search.subscribe((val:any) => {
+    this.mediaService.search.subscribe((val: any) => {
       this.searchKey = val;
     });
   }
@@ -36,17 +37,31 @@ export class EventComponent implements OnInit {
     });
 
     this.mediaService.all().subscribe((res: Response) => {
-          this.events = res.data;
+      this.events = res.data;
+      this.filterCategory = res.data;
+      this.events.forEach((a: any) => {
+        this.categories.forEach((c: any) => {
+          if (a.category === c.id) {
+            a.category = c.name;
+          }
         });
-    
+      });
+      console.log(this.events);
+    });
   }
 
-  search(event:any) {
+  search(event: any) {
     this.searchTerm = (event.target as HTMLInputElement).value;
     console.log(this.searchTerm);
     this.mediaService.search.next(this.searchTerm);
   }
 
-  
-
+  filter(category: string) {
+    this.filterCategory = this.events
+    .filter((a:any) => {
+      if (a.category == category || category == '') {
+        return a;
+      }
+    });
+  }
 }
