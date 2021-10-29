6f75614b6617953c4bcecb8c374cbb3e7e370f61
Bz-Carter <lebz2016@gmail.com>
2021-10-29 18:22:14 +0000

    envent single created .

--- src/app/pages/event/single/single.component.ts
+++ src/app/pages/event/single/single.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Media } from 'src/app/interfaces/media';
+import { Category } from 'src/app/interfaces/category';
+import { Response } from 'src/app/interfaces/response';
+import { Video } from 'src/app/interfaces/video';
+import { Photo } from 'src/app/interfaces/photo';
+import { MediaService } from 'src/app/services/media.service';
+import { CategoryService } from 'src/app/services/category.service';
+import { VideoService } from 'src/app/services/video.service';
+import { PhotoService } from 'src/app/services/photo.service';
 
 @Component({
   selector: 'app-single',
@@ -7,9 +17,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SingleComponent implements OnInit {
 
-  constructor() { }
+  event: Media;
+  categories: Category[] = [];
+  videos: Video[] = [];
+  photos: Photo[] = [];
+
+
+  constructor(
+    private categoryService: CategoryService,
+    private MediaService: MediaService,
+    private videoService: VideoService,
+    private photoService: PhotoService,
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    this.videoService.all().subscribe((res: Response) => {
+      this.videos = res.data;
+    });
+
+    this.categoryService.all().subscribe((res: Response) => {
+      this.categories = res.data;
+    });
+
+    this.photoService.all().subscribe((res: Response) => {
+      this.photos = res.data;
+    });
+
+    this.route.params.subscribe((params) => {
+      this.MediaService.get(params.id).subscribe((res: Response) => {
+        this.event = res.data;
+      });
+    });
   }
 
 }
