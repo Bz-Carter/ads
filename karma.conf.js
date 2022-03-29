41c43ee4e347c900f28cef18cecc902ea099c6be
Bz-Carter <lebz2016@gmail.com>
2022-03-29 11:19:11 +0000

    rename project .

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/wsearch'),
+      dir: require('path').join(__dirname, './coverage/ads'),
       subdir: '.',
       reporters: [
         { type: 'html' },
